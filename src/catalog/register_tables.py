da507af81ab8796842a8ba050ea759c3f7270d83
Shivani Chahal <shivanichahal@Shivanis-MacBook-Air.local>
2026-01-05 01:31:34 -0800

    code to create tables

--- src/catalog/register_tables.py
+++ src/catalog/register_tables.py
@@ -1,81 +1,94 @@
+from config.paths import (
+    BRONZE_PATH,
+    SILVER_PASS_PATH,
+    SILVER_FAIL_PATH,
+    MDM_PATH,
+    GOLD_PATH,
+    STEWARD_LOG_PATH
+)
+
+DATABASE = "nyc_taxi_lake"
+
+
 def register_all_tables(spark):
     """
-    Registers all Delta Lake tables in AWS Glue Data Catalog.
+    Registers all Delta tables in AWS Glue Data Catalog
+    using centralized path configuration.
     Safe to run multiple times.
     """
 
     # --------------------------------------------------
     # Create Database
     # --------------------------------------------------
-    spark.sql("""
-        CREATE DATABASE IF NOT EXISTS nyc_taxi
+    spark.sql(f"""
+        CREATE DATABASE IF NOT EXISTS {DATABASE}
     """)
 
     # --------------------------------------------------
-    #  Bronze Tables
+    # Bronze
     # --------------------------------------------------
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.bronze_yellow_taxi
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.bronze_yellow_taxi
         USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/bronze/yellow_taxi'
+        LOCATION '{BRONZE_PATH}'
     """)
 
     # --------------------------------------------------
-    #  Silver Tables
+    # Silver
     # --------------------------------------------------
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.silver_yellow_taxi_pass
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.silver_yellow_taxi_pass
         USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/silver/pass/yellow_taxi'
+        LOCATION '{SILVER_PASS_PATH}'
     """)
 
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.silver_yellow_taxi_fail
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.silver_yellow_taxi_fail
         USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/silver/fail/yellow_taxi'
-    """)
-
-    # --------------------------------------------------
-    #  MDM Tables
-    # --------------------------------------------------
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.location_master
-        USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/mdm/location_master'
+        LOCATION '{SILVER_FAIL_PATH}'
     """)
 
     # --------------------------------------------------
-    #  Gold – Quality Metrics
+    #MDM
     # --------------------------------------------------
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.quality_completeness
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.location_master
         USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/gold/quality_metrics/completeness'
-    """)
-
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.quality_accuracy
-        USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/gold/quality_metrics/accuracy'
-    """)
-
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.quality_timeliness
-        USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/gold/quality_metrics/timeliness'
-    """)
-
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.quality_consistency
-        USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/gold/quality_metrics/consistency'
+        LOCATION '{MDM_PATH}'
     """)
 
     # --------------------------------------------------
-    #Governance / Steward Logs
+    # Gold – Quality Metrics
     # --------------------------------------------------
-    spark.sql("""
-        CREATE TABLE IF NOT EXISTS nyc_taxi.steward_activity_log
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.quality_completeness
         USING DELTA
-        LOCATION 's3://nyc-taxi-lakehouse-shivani-bucket/src/governance/steward_activity'
+        LOCATION '{GOLD_PATH}/completeness'
+    """)
+
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.quality_accuracy
+        USING DELTA
+        LOCATION '{GOLD_PATH}/accuracy'
+    """)
+
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.quality_timeliness
+        USING DELTA
+        LOCATION '{GOLD_PATH}/timeliness'
+    """)
+
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.quality_consistency
+        USING DELTA
+        LOCATION '{GOLD_PATH}/consistency'
+    """)
+
+    # --------------------------------------------------
+    # Governance / Steward Logs
+    # --------------------------------------------------
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {DATABASE}.steward_activity_log
+        USING DELTA
+        LOCATION '{STEWARD_LOG_PATH}'
     """)
