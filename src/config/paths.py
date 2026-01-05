3e448289b0702071b9722bfb74de6aecb8362f0d
Shivani Chahal <shivanichahal@Shivanis-MacBook-Air.local>
2026-01-04 22:00:12 -0800

    update change

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,8 +1,8 @@
 RAW_PATH = "s3://nyc-taxi-lakehouse-shivani/raw/yellow_tripdata_2025-08.parquet"
 BRONZE_PATH = "s3://nyc-taxi-lakehouse-shivani/bronze/yellow_taxi"
-SILVER_PATH = "s3://nyc-taxi-lakehouse-shivanisilver/yellow_taxi"
+SILVER_PASS_PATH = "s3://nyc-taxi-lakehouse-bucket/silver/pass/yellow_taxi"
+SILVER_FAIL_PATH = "s3://nyc-taxi-lakehouse-bucket/silver/fail/yellow_taxi"
 MDM_PATH = "s3://nyc-taxi-lakehouse-shivani/mdm/location_master"
 GOLD_PATH = "s3://nyc-taxi-lakehouse-shivani/gold/quality_metrics"
-STEWARD_LOG_PATH = (
-    "s3://nyc-taxi-lakehouse-shivani/governance/steward_activity"
-)
+STEWARD_LOG_PATH = "s3://nyc-taxi-lakehouse-shivani/governance/steward_activity"
+
--- src/silver/silver_transform.py
+++ src/silver/silver_transform.py
@@ -1,8 +1,15 @@
 from pyspark.sql.functions import *
 from silver.dq_rules import apply_dq_rules
-from config.paths import BRONZE_PATH, SILVER_PATH
+from config.paths import BRONZE_PATH, SILVER_PASS_PATH, SILVER_FAIL_PATH
 
 def build_silver(spark):
     df = spark.read.format("delta").load(BRONZE_PATH)
-    df = apply_dq_rules(df).filter(col("dq_status") == "PASS")
-    df.write.format("delta").mode("overwrite").save(SILVER_PATH)
+    df = apply_dq_rules(df)
+
+    # PASS
+    df.filter(col("dq_status") == "PASS") \
+      .write.format("delta").mode("overwrite").save(SILVER_PASS_PATH)
+
+    # FAIL (with reasons)
+    df.filter(col("dq_status") == "FAIL") \
+      .write.format("delta").mode("overwrite").save(SILVER_FAIL_PATH)
